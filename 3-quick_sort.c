43cd4887a9f230da7d43feff9a9b5f41fa840187
Yasgc24 <4537@holbertonschool.com>
2022-06-21 20:51:09 -0500

    rask 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -2,8 +2,6 @@
 
 /**
  * swap_int - function that swap two integers
- * @array: array of elements
- * @size: size of the array
  * @a: first integer to swap
  * @b: second integer to swap
  * Return: void
@@ -11,11 +9,11 @@
 
 void swap_int(int *a, int *b)
 {
-    int temp;
+	int temp;
 
-    temp = *a;
-    *a = *b;
-    *b = temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
 }
 
 /**
@@ -32,48 +30,48 @@ void swap_int(int *a, int *b)
 
 int partition(int *array, size_t size, int low, int high)
 {
-    int pivot = array[high];
-    int i = (low - 1), j;
+	int pivot = array[high];
+	int i = (low - 1), j;
 
-    for (j = low; j <= high - 1; j++)
-    {
-        if (array[j] <= pivot)
-        {
-            i++;
-            if (i != j)
-            {
-                swap_int(&array[i], &array[j]);
-                print_array(array, size);
-            }
-        }
-    }
-    if (i + 1 != high)
-    {
-        swap_int(&array[i + 1], &array[high]);
-        print_array(array, size);
-    }
-    return(i + 1);
+	for (j = low; j <= high - 1; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				swap_int(&array[i], &array[j]);
+				print_array(array, size);
+			}
+		}
+	}
+	if (i + 1 != high)
+	{
+		swap_int(&array[i + 1], &array[high]);
+		print_array(array, size);
+	}
+	return (i + 1);
 }
 
 /**
- * quick_sortp - 
+ * quick_sortp - quicksort algorithm through recursion
  * @array: array of integers to be sorted
  * @size: size of the array
  * @low: starting index
- * @high: ending index 
+ * @high: ending index
  */
 
 void quick_sortp(int *array, size_t size, int low, int high)
 {
-    int idxpart;
+	int idxpart;
 
-    if (low < high)
-    {
-        idxpart = partition(array, size, low, high);
+	if (low < high)
+	{
+		idxpart = partition(array, size, low, high);
 
-        quick_sortp(array, size, low, idxpart - 1);
-        quick_sortp(array, size, idxpart + 1, high);
-    }
+		quick_sortp(array, size, low, idxpart - 1);
+		quick_sortp(array, size, idxpart + 1, high);
+	}
 }
 
 /**
@@ -85,8 +83,8 @@ void quick_sortp(int *array, size_t size, int low, int high)
 
 void quick_sort(int *array, size_t size)
 {
-    if (array == NULL || size < 2)
-        return;
-    
-    quick_sortp(array, size, 0, size - 1);
+	if (array == NULL || size < 2)
+		return;
+
+	quick_sortp(array, size, 0, size - 1);
 }
